694440a927b5919d410c234c4c0f00e743fecc75
Daniil Dankovskyi <daniil@example.com>
2025-05-13 19:43:13 +0200

    init

--- backend_api/app/applications/auth/password_handler.py
+++ backend_api/app/applications/auth/password_handler.py
@@ -8,3 +8,7 @@ class PasswordEncrypt:
     @classmethod
     async def get_password_hash(cls, password: str) -> str:
         return cls.pwd_context.hash(password)
+
+    @classmethod
+    async def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
+        return cls.pwd_context.verify(plain_password, hashed_password)
