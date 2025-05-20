b79091fbf1e30a96e222843e159d745baf108794
Daniil Dankovskyi <daniil@example.com>
2025-05-20 18:22:44 +0200

    intro

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -15,3 +15,10 @@ async def user_login(
 ):
     token_pair = await auth_handler.get_login_token_pairs(data, session)
     return token_pair
+
+
+
+
+@router_auth.get("/get-my-info")
+async def get_my_info():
+    pass
