149aa5942a39e5510e673537c4cf23f51033a67a
Daniil Dankovskyi <daniil@example.com>
2025-05-20 19:37:35 +0200

    intro

--- backend_api/app/applications/auth/router.py
+++ backend_api/app/applications/auth/router.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from applications.auth.auth_handler import auth_handler
 from applications.auth.security import get_current_user
 from applications.users.models import User
+from applications.users.schemas import BaseUserInfo
 from database.session_dependencies import get_async_session
 
 router_auth = APIRouter()
@@ -19,10 +20,6 @@ async def user_login(
     return token_pair
 
 
-
-
 @router_auth.get("/get-my-info")
-async def get_my_info(
-        user: User = Depends(get_current_user)
-):
-    pass
+async def get_my_info(user: User = Depends(get_current_user)) -> BaseUserInfo:
+    return user
--- backend_api/app/applications/auth/security.py
+++ backend_api/app/applications/auth/security.py
@@ -1,12 +1,11 @@
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.auth.auth_handler import auth_handler
 from applications.users.crud import get_user_by_email
 from applications.users.models import User
-from sqlalchemy.ext.asyncio import AsyncSession
 from database.session_dependencies import get_async_session
-
 
 
 class SecurityHandler:
@@ -18,5 +17,7 @@ async def get_current_user(
     session: AsyncSession = Depends(get_async_session),
 ) -> User:
     payload = await auth_handler.decode_token(token)
-    user = await get_user_by_email(user_email, session)
-    pass
+    user = await get_user_by_email(payload["user_email"], session)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
