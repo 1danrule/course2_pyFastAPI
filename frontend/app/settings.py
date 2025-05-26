8074687a45a3d4d4e6123617bdc74ea97d47bbc1
Daniil Dankovskyi <daniil@example.com>
2025-05-26 19:03:25 +0200

    nginx conf

---
+++ frontend/app/settings.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    DEBUG: bool = False
+
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
+
+
+settings = get_settings()
