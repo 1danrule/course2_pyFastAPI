807c8a75692fe3ac326e9c5c24ad11a8f1e4b6e6
Daniil Dankovskyi <daniil@example.com>
2025-06-03 20:03:18 +0200

    register user

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Form, Depends, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-from backend_api.api import get_current_user_with_tokens, login_user
+from backend_api.api import get_current_user_with_tokens, login_user, register_user
 
 router = APIRouter()
 
@@ -73,14 +73,12 @@ async def register(
         response.delete_cookie('access_token')
         return response
 
-    user_tokens = await login_user(user_email, password)
-    access_token = user_tokens.get('access_token')
-    if not access_token:
-        errors = ["Incorrect login or password"]
-        context["errors"] = errors
-        return templates.TemplateResponse('login.html', context=context)
+    created_user = await register_user(user_email=user_email, password=password, name=user_name)
+    if created_user['email']:
 
-
-    response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
-    response.set_cookie(key="access_token", value=access_token, httponly=True, max_age=60*5)
-    return response
+        user_tokens = await login_user(user_email, password)
+        access_token = user_tokens.get('access_token')
+    #     context["errors"] = errors
+        response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(key="access_token", value=access_token, httponly=True, max_age=60*5)
+        return response
