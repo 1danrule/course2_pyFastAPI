f5c079d7f8b329bb744be20fdb3597be438c05a6
Daniil Dankovskyi <daniil@example.com>
2025-06-17 18:07:48 +0200

    email sending

--- notification_service/app/main.py
+++ notification_service/app/main.py
@@ -1,1 +1,11 @@
-print(55555555555555555555)
+from services.rabbit.rabbitmq_service import rabbitmq_broker
+
+
+def main():
+    with rabbitmq_broker.get_connection() as connection:
+        with connection.channel() as channel:
+            rabbitmq_broker.consume_message(channel)
+
+
+if __name__ == "__main__":
+    main()
