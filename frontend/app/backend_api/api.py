61fa606dc1c7e09ab0af36d03e9ecd6e9efa6b60
Daniil Dankovskyi <daniil@example.com>
2025-06-24 18:51:30 +0200

    extract products front 1

--- frontend/app/backend_api/api.py
+++ frontend/app/backend_api/api.py
@@ -21,7 +21,6 @@ async def register_user(user_email: str, password: str, name: str):
             headers={'Content-Type': 'application/json'}
 
         )
-        print(response.json(), 88888888888)
         return response.json()
 
 async def get_current_user_with_tokens(request: Request) -> dict:
@@ -39,5 +38,14 @@ async def get_user_info(access_token: str):
             url=f'{settings.BACKEND_API}auth/get-my-info',
             headers={"Authorization": f'Bearer {access_token}'}
 
+        )
+        return response.json()
+
+
+async def get_products(q: str = ""):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url=f'{settings.BACKEND_API}products/',
+            params={"q": q}
         )
         return response.json()
--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Form, Depends, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-from backend_api.api import get_current_user_with_tokens, login_user, register_user
+from backend_api.api import get_current_user_with_tokens, login_user, register_user, get_products
 
 router = APIRouter()
 
@@ -9,7 +9,17 @@ templates = Jinja2Templates(directory='templates')
 
 @router.get('/')
 async def index(request: Request, user: dict=Depends(get_current_user_with_tokens)):
-    context = {'request': request}
+    products = await get_products()
+
+
+
+
+
+
+
+
+    context = {'request': request, "products": products}
+    print(products, 555555555555555)
     if user.get('name'):
         context['user'] = user
     response = templates.TemplateResponse('index.html', context=context)
