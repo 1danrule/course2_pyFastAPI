fa5615e26602eadcbe1a02a76639607cb3e76321
Daniil Dankovskyi <daniil@example.com>
2025-07-01 18:58:58 +0200

    cart 3

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from applications.auth.router import router_auth
 from applications.users.router import router_users
-from applications.products.router import products_router
+from applications.products.router import products_router, cart_router
 from settings import settings
 
 sentry_sdk.init(
@@ -17,4 +17,5 @@ def get_application() -> FastAPI:
     app.include_router(router_users, prefix="/users", tags=["Users"])
     app.include_router(router_auth, prefix="/auth", tags=["Auth"])
     app.include_router(products_router, prefix="/products", tags=["Products"])
+    app.include_router(cart_router, prefix="/carts", tags=["Cart"])
     return app
